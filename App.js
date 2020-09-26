6064eff7dc1058805ebc133ce368f6bab666115b
Carts06 <johann_carta@dlsu.edu.ph>
2020-09-26 18:12:22 +0800

    lol i tried

--- App.js
+++ App.js
@@ -1,9 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import "react-native-gesture-handler";
+import Login from "./src/Login.js";
+import Reporting from "./src/Screens/Reporting.js";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import Login from './src/Login'
+const Stack = createStackNavigator();
+//const bottomTab = createMaterialBottomTabNavigator();
 
+export default class App extends Component {
+  Auth = () => (
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} />
+    </Stack.Navigator>
+  );
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" children={Login} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
+/*
 export default function App() {
   return (
     <View style={styles.container}>
@@ -11,12 +34,12 @@ export default function App() {
     </View>
   );
 }
-
+*/
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
