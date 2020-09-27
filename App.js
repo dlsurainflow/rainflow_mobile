6a08f44147304c1ed40fbc2ef60612d8bb5d98b3
Carts06 <johann_carta@dlsu.edu.ph>
2020-09-27 12:37:15 +0800

    Error fix

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text } from "react-native";
 import "react-native-gesture-handler";
 import LoginScreen from "./src/Login.js";
 import ReportingScreen from "./src/Screens/Reporting.js";
@@ -46,11 +46,3 @@ export default function App() {
   );
 }
 */
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
